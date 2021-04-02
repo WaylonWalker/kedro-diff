6f9cce3737eedbbc74d943c6879e2fff4f599b33
Waylon Walker <walker_waylon_s@cat.com>
2021-04-02 16:10:20 -0500

    init kedro_diff

---
+++ kedro_diff/__init__.py
@@ -0,0 +1,16 @@
+"""Kedro Diff - quickly diff kedro history
+
+kedro diff aims to be a familiar interface into comparing two points in
+history. Git diffs are fantastic tools but often are too granular to see what
+has changed inside the pipeline. kedro diff aims to be a familiar tool at a
+higher level so we can see changes to nodes (names, inputs, outputs, tags).
+
+HomePage and documentation: https://diff.kedro.dev/
+
+Copyright (c) 2020, Waylon Walker.
+License: MIT (see LICENSE for details)
+"""
+__version__ = "0.0.0"
+__author__ = ("Waylon Walker",)
+__author_email__ = ("waylon@waylonwalker.com",)
+__license__ = "MIT"
