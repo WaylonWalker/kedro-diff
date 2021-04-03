b6229fd317d4d10a9fb4e91003449a75e102f0ab
Waylon Walker <walker_waylon_s@cat.com>
2021-04-03 00:40:41 -0500

    add --stat option

--- kedro_diff/cli.py
+++ kedro_diff/cli.py
@@ -1,5 +1,7 @@
 """kedro_diff cli module."""
+import json
 import logging
+from pathlib import Path
 from typing import IO, TYPE_CHECKING, Tuple
 
 import click
@@ -97,8 +99,11 @@ def get_json(
     help="verbosity level, -v enables diff related logs, -vv enables all logs",
 )
 @click.argument("commit", nargs=-1)
+@click.option("--stat", is_flag=True, help="generate short stats only")
 @click.pass_obj
-def diff(metadata: "ProjectMetadata", verbose: int, commit: Tuple[str, ...]) -> None:
+def diff(
+    metadata: "ProjectMetadata", verbose: int, commit: Tuple[str, ...], stat: bool
+) -> None:
     """Diff two commits."""
     from kedro_diff.commit_parser import parse_commit
     from kedro_diff.get_pipelines import to_json
@@ -106,6 +111,25 @@ def diff(metadata: "ProjectMetadata", verbose: int, commit: Tuple[str, ...]) -> 
     commit1, commit2 = parse_commit(commit)
     to_json(metadata.project_path, commit1)
     to_json(metadata.project_path, commit2)
+    if stat:
+        pipe1 = json.loads((Path(".kedro-diff") / (commit1 + ".json")).read_text())[
+            "pipeline"
+        ]
+        pipe2 = json.loads((Path(".kedro-diff") / (commit2 + ".json")).read_text())[
+            "pipeline"
+        ]
+        if len(pipe1) != len(pipe2):
+            new_nodes = set([node["name"] for node in pipe2]).difference(
+                set([node["name"] for node in pipe1])
+            )
+
+            dropped_nodes = set([node["name"] for node in pipe1]).difference(
+                set([node["name"] for node in pipe2])
+            )
+
+            print(
+                f'[red]M[/red] __default__ | {len(new_nodes) + len(dropped_nodes)} [green]{"+" * len(new_nodes)}[/green][red]{"-"*len(dropped_nodes)}[/red]'
+            )
 
 
 if __name__ == "__main__":
