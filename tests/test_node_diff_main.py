8156869eb5c2c236b0a1267ab98ed35ed7440fd2
Waylon Walker <quadmx08@gmail.com>
2021-05-29 12:20:21 -0500

    FIX issue with __main__

---
+++ tests/test_node_diff_main.py
@@ -0,0 +1,18 @@
+should_prints = [
+    "name_changed",
+    "input_changed",
+    "output_changed",
+    "new_node",
+    "deleted_node",
+    "inputs",
+    "outputs",
+]
+
+
+def test_node_diff_main(capsys, caplog):
+    from kedro_diff.node_diff import __main__
+
+    assert caplog.text == ""
+    captured = capsys.readouterr()
+    for should_print in should_prints:
+        assert should_print in captured.out
